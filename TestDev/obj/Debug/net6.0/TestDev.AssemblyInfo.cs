26d4a71aad1afc243ac224cce0dfe0675148135b
mauro paredes <m.paredes943@gmail.com>
2023-12-02 14:54:17 -0300

    logica en el front para no tocar clienteComodin

--- TestDev/obj/Debug/net6.0/TestDev.AssemblyInfo.cs
+++ TestDev/obj/Debug/net6.0/TestDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3416bac28c033269a3414001ed0eef2aadf2d2d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8bd63157c9e8087dc4bf31d775abe73cc488193")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
